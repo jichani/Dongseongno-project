22b422b9505d180a2f7fd186bb3d270413531532
jichani <jichan7571@naver.com>
2023-07-19 14:05:06 +0900

    애니메이션 주기 전

---
+++ main_carousel.js
@@ -0,0 +1,18 @@
+const progressCircle = document.querySelector(".autoplay-progress svg");
+const progressContent = document.querySelector(".autoplay-progress span");
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  effect: 'fade', // 페이드 효과 사용
+  centeredSlides: true,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = `${Math.ceil(time / 1000)}`;
+    }
+  }
+});
