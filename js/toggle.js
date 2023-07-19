c3a9c694addfe998c29c65df0680834d52ed4f67
jichani <jichan7571@naver.com>
2023-07-19 22:19:10 +0900

    add

---
+++ js/toggle.js
@@ -0,0 +1,14 @@
+const mainMenuTitle = document.querySelectorAll(".main_menu_title");
+const subMenues = document.querySelectorAll(".sub_menu");
+
+console.log(subMenues);
+for (let i = 0; i < mainMenuTitle.length; i++) {
+  mainMenuTitle[i].addEventListener("click", () => {
+    subMenues[i].classList.toggle("show");
+    for (let j = 0; j < mainMenuTitle.length; j++) {
+      if (i !== j) {
+        subMenues[j].classList.remove("show");
+      }
+    }
+  });
+}
