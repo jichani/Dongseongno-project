86d579b5cd1b1b19ad476534326524a9b891947c
jichani <jichan7571@naver.com>
2023-07-20 12:42:12 +0900

    edit

--- js/second_carousel.js
+++ js/second_carousel.js
@@ -10,6 +10,23 @@ var swiper = new Swiper(".second-swiper", {
     delay: 4000,
     disableOnInteraction: true,
   },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  breakpoints: {
+    380: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
   mousewheel: false,
   keyboard: true,
 
