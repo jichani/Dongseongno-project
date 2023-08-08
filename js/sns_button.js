1b6df262ecf68b98c355fbd286a2f043cf3d66e6
jichani <jichan7571@naver.com>
2023-08-08 23:52:43 +0900

    sns 페이지 반응형 끝!

--- js/sns_button.js
+++ js/sns_button.js
@@ -4,5 +4,10 @@ tagButtons.forEach(button => {
   button.addEventListener('click', () => {
     tagButtons.forEach(btn => btn.classList.remove('active'));
     button.classList.add('active');
+    // console.dir(button);
+    // console.dir(button.dataset.tag);
+    const tag = button.dataset.tag;
+    optionParams.q = `동성로${tag}`;
+    // performYoutubeSearch();
   });
 });
